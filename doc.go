d8a3142dd2a5c4eb8ca05c91d7df3cd3bf87d785
armfazh <armfazh@cloudflare.com>
2022-06-02 13:43:51 -0700

    Nits on version number.

--- doc.go
+++ doc.go
@@ -5,7 +5,7 @@ arbitrary-length byte strings and output a point on an elliptic curve.
 These methods follow the current work-in-progress standardization effort
 at IETF https://datatracker.ietf.org/doc/draft-irtf-cfrg-hash-to-curve.
 
-Version: draft-irtf-cfrg-hash-to-curve-05
+Version: draft-irtf-cfrg-hash-to-curve-14
 
 */
 package h2c
